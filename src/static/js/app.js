570b99e58143473bb205d0f8939fb7eb96f50a86
seapp <seapp88@icloud.com>
2020-12-28 20:28:12 +0300

    Update Placeholder

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="Новая заметка"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
